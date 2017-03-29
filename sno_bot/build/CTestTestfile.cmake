108d777cfbfd0978815ea2a14b9fc4cae9b3f7c4
Rob Ressler <doublerob7@gmail.com>
2017-03-29 15:55:48 -0600

    built folder directory for use with catkin/ros

---
+++ sno_bot/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robert/sno_bot/src
+# Build directory: /home/robert/sno_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(base_controller)
---
+++ sno_bot/build/base_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robert/sno_bot/src/base_controller
+# Build directory: /home/robert/sno_bot/build/base_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
